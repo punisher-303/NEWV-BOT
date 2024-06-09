5b4e6d8be1201b659d8c489ed09cc0d23d66249e
punisher-303 <mehafin0@gmail.com>
2024-06-09 18:28:56 +0530

    Update config.py

--- plugins/helpers/config.py
+++ plugins/helpers/config.py
@@ -21,7 +21,6 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN")
 MDISK_API = os.environ.get("MDISK_API")
 MDISK_CHANNEL = list(int(i.strip()) for i in os.environ.get("MDISK_CHANNEL").split(" ")) if os.environ.get("CHANNEL_ID") else []
 FORWARD_MESSAGE = bool(os.environ.get("FORWARD_MESSAGE"))
-ADMINS = list(int(i.strip()) for i in os.environ.get("ADMINS").split(",")) if os.environ.get("ADMINS") else []
 SOURCE_CODE = "💕SHARE AND SUPPORT💕"
 CHANNELS = bool(os.environ.get("CHANNELS"))
 
