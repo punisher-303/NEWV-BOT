88a14fc162d1c629d0fa7c102c582e4440e6c683
punisher-303 <mehafin0@gmail.com>
2024-06-09 14:49:14 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /NEWV-BOT
 fi
-cd /THEMOVIEBOT
+cd /NEWV-BOT
 pip3 install -U -r requirements.txt
-echo "Starting THEMOVIEBOT...."
+echo "Starting NEWVBOT...."
 python3 bot.py
