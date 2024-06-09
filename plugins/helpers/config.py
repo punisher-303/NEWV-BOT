3f15375520c23bf39c8bab9187eea0aedc6a8488
punisher-303 <mehafin0@gmail.com>
2024-06-09 17:32:53 +0530

    Update config.py

--- plugins/helpers/config.py
+++ plugins/helpers/config.py
@@ -8,7 +8,7 @@ load_dotenv()
 API_ID = int(environ.get("API_ID", ""))
 API_HASH = environ.get("API_HASH", "")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
-ADMINS = (environ.get("ADMINS", "")          
+ADMINS = environ.get("ADMINS", "")          
 CAPTION = environ.get("CAPTION", "")
 
 # for thumbnail ( back end is MrMKN brain 😉)
