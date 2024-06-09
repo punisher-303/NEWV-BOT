9e0c175482cf308c96addabd16d87962b18cf10f
punisher-303 <mehafin0@gmail.com>
2024-06-09 14:00:10 +0530

    Update Approve.py

--- plugins/private/Approve.py
+++ plugins/private/Approve.py
@@ -44,7 +44,7 @@ APPROVED = environ.get("APPROVED_WELCOME", "on").lower()
 @Client.on_message(filters.private & filters.command(["aprv"]))
 async def start(client: pr0fess0r_99, message: Message):
     approvedbot = await client.get_me() 
-    button = [[ InlineKeyboardButton("📦 Repo", url="https://github.com/PR0FESS0R-99/Auto-Approved-Bot"), InlineKeyboardButton("Updates 📢", url="t.me/Mo_Tech_YT") ],
+    button = [[ InlineKeyboardButton("📦 Repo", url="https://t.me/valkyraegfx"), InlineKeyboardButton("Updates 📢", url="t.me/valkyraemovies") ],
               [ InlineKeyboardButton("➕️ Add Me To Your Chat ➕️", url=f"http://t.me/{approvedbot.username}?startgroup=botstart") ]]
     await client.send_message(chat_id=message.chat.id, text=f"**__Hello {message.from_user.mention} Iam Auto Approver Join Request Bot Just [Add Me To Your Group Channnl](http://t.me/{approvedbot.username}?startgroup=botstart) || Repo https://github.com/PR0FESS0R-99/Auto-Approved-Bot||**__", reply_markup=InlineKeyboardMarkup(button), disable_web_page_preview=True)
 
@@ -56,7 +56,7 @@ async def autoapprove(client: pr0fess0r_99, message: ChatJoinRequest):
     await client.approve_chat_join_request(chat_id=chat.id, user_id=user.id)
     if APPROVED == "on":
         buttons = [[
-            InlineKeyboardButton('🧩𝐉𝐎𝐈𝐍 𝐆𝐑𝐎𝐔𝐏🧩', url=f'https://t.me/nasrani_update')
+            InlineKeyboardButton('🧩𝐉𝐎𝐈𝐍 𝐆𝐑𝐎𝐔𝐏🧩', url=f'https://t.me/valkyraemovies')
             
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
