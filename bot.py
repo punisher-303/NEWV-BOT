3fd414148c7b987c12e1a91e843a29f0ad2c9a14
punisher-303 <mehafin0@gmail.com>
2024-06-09 19:37:30 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -27,7 +27,7 @@ class Bot(Client):
 
     def __init__(self):
         super().__init__(
-            name=SESSION,
+            name="Professor-Bot",
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
