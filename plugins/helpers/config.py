86bd3c79ef77b81f84bd289f1f446dc8b685e7ce
punisher-303 <mehafin0@gmail.com>
2024-06-09 17:44:48 +0530

    Update config.py

--- plugins/helpers/config.py
+++ plugins/helpers/config.py
@@ -8,7 +8,7 @@ load_dotenv()
 API_ID = int(environ.get("API_ID", ""))
 API_HASH = environ.get("API_HASH", "")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
-ADMINS = environ.get("ADMINS", "")          
+ADMINS = int(environ.get("ADMINS", ""))          
 CAPTION = environ.get("CAPTION", "")
 
 # for thumbnail ( back end is MrMKN brain 😉)
@@ -22,7 +22,7 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN")
 MDISK_API = os.environ.get("MDISK_API")
 MDISK_CHANNEL = list(int(i.strip()) for i in os.environ.get("MDISK_CHANNEL").split(" ")) if os.environ.get("CHANNEL_ID") else []
 FORWARD_MESSAGE = bool(os.environ.get("FORWARD_MESSAGE"))
-ADMINS = list((i.strip()) for i in os.environ.get("ADMINS").split(",")) if os.environ.get("ADMINS") else []
+ADMINS = list(int(i.strip()) for i in os.environ.get("ADMINS").split(",")) if os.environ.get("ADMINS") else []
 SOURCE_CODE = "💕SHARE AND SUPPORT💕"
 CHANNELS = bool(os.environ.get("CHANNELS"))
 
