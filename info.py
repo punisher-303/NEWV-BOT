07270320735871b169ec88842b84dffc36175bc6
punisher-303 <mehafin0@gmail.com>
2024-06-09 19:09:58 +0530

    Update info.py

--- info.py
+++ info.py
@@ -16,6 +16,7 @@ SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ['API_ID'])
 API_HASH = environ['API_HASH']
 BOT_TOKEN = environ['BOT_TOKEN']
+SOURCE_CODE = "💕SHARE AND SUPPORT💕"
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
